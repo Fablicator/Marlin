340761e83fd258897dfb738c3ee3adf6637e483c
Karl Calden <kcalden323@gmail.com>
2019-10-28 11:37:13 -0400

    Increase Z Homing speed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1160,7 +1160,7 @@ These are values that are specific to each printer.
 
 // Homing speeds (mm/m)
 #define HOMING_FEEDRATE_XY (50*60)
-#define HOMING_FEEDRATE_Z  (4*60)
+#define HOMING_FEEDRATE_Z  (10*60)
 
 // @section calibrate
 
