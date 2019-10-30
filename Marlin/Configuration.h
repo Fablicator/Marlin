b7ee2cc40f84d0950d7e8c7ed605aaf48ac8ee5e
Karl Calden <kcalden323@gmail.com>
2019-10-30 18:43:14 -0400

    Lowered Z max speed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -670,7 +670,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 400, 350, 200, 20 }
+#define DEFAULT_MAX_FEEDRATE          { 400, 350, 45, 20 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
