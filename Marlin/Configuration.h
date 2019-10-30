4bb53a9b233f60ee00d8ae643b9a82cc9e09c118
Karl Calden <kcalden323@gmail.com>
2019-10-30 16:32:26 -0400

    Moved all tunable parameters to the top

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1,3 +1,41 @@
+//============================================================================
+//=========================== DEFINE PRINTER MODEL ===========================
+//============================================================================
+
+// UNCOMMMENT USING CTRL+/ TO SELECT THE PRINTER YOU ARE USING
+// THIS WILL CHANGE THE CONFIGURATION TO MATCH THE PRINTER
+
+// #define USING_FM1
+// #define USING_SX
+#define USING_MX
+
+//=============================================================================
+//============================= CALIBRATE PRINTER =============================
+//=============================================================================
+
+
+#define Z_MAX_POS 251.45             // Decrease this value if first layer is too close to the print bed.
+#define HOTEND_OFFSET_Y {0.0, 0.1}  // (0, X in mm) If the right extruder is printing too far towards the front of the machine, decrease this number
+#define X1_BED_OFFSET   9.5     // Distance between left extruder tip and left side of bed when it's at the home position
+#define X2_BED_OFFSET   17.40   // If the right extruder is printing too far to the right, increase this value
+
+// EXTRUDER PID TUNING
+// USE "M303 C8 E0 S240" TO FIND VALUES AUTOMATICALLY
+#define  DEFAULT_Kp 11.44
+#define  DEFAULT_Ki 0.9
+#define  DEFAULT_Kd 36.31
+
+#define TEMP_SENSOR_BED 16          // Defines what thermistor we are using on the bed (17 for the green thermistor, 16 for the new blue thermistor)
+#define FILAMENT_RUNOUT_SENSOR    // Uncomment this to enable filament out sensors.
+
+
+
+
+
+//============================================================================
+//================== DON'T CHANGE ANYTHING BELOW THIS POINT ==================
+//============================================================================
+
 /**
  * Marlin 3D Printer Firmware
  * Copyright (C) 2016 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
@@ -39,17 +77,8 @@
 #define CONFIGURATION_H
 #define CONFIGURATION_H_VERSION 010109
 
-/*
-These are values that are specific to each printer.
-*/
-#define Z_MAX_POS 251.45             // Decrease this value if first layer is too close to the print bed.
-#define HOTEND_OFFSET_Y {0.0, 0.1}  // (0, X in mm) If the right extruder is printing too far towards the front of the machine, decrease this number
-#define X1_BED_OFFSET   9.5     // Distance between left extruder tip and left side of bed when it's at the home position
-#define X2_BED_OFFSET   17.30   // Distance between right extruder tip and right side of bed when it's at the home position
 #define MIN_EXTRUDER_DISTANCE 34 // Minimum distance between the tips of the nozzles
 
-#define TEMP_SENSOR_BED 16          // Defines what thermistor we are using on the bed (17 for the green thermistor, 16 for the new blue thermistor)
-#define FILAMENT_RUNOUT_SENSOR    // Uncomment this to enable filament out sensors.
 #if DISABLED(USING_FM1) && DISABLED(USING_SX) && DISABLED(USING_MX)
   #error "No printer defined! Please define a printer at the top of the Configuration.h!"
 #endif
@@ -399,9 +428,9 @@ These are values that are specific to each printer.
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Fablicator
-  #define  DEFAULT_Kp 11.44
-  #define  DEFAULT_Ki 0.9
-  #define  DEFAULT_Kd 36.31
+  // #define  DEFAULT_Kp 11.44
+  // #define  DEFAULT_Ki 0.9
+  // #define  DEFAULT_Kd 36.31
 
   // Ultimaker
   // #define DEFAULT_Kp 22.2
