8ffba688ac458bf0f4aa5f25cf367999e1c9c283
Karl Calden <kcalden323@gmail.com>
2019-10-30 13:40:31 -0400

    Added X0_BED_OFFSET

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -890,14 +890,15 @@ These are values that are specific to each printer.
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 352
-#define Y_BED_SIZE 215
+#define X_BED_SIZE      342
+#define Y_BED_SIZE      215
+#define X0_BED_OFFSET   9.5 // Distance between left extruder tip and bed when it's at the home position
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
+#define X_MAX_POS (X_BED_SIZE + X0_BED_OFFSET)
 #define Y_MAX_POS Y_BED_SIZE
 
 /**
