846983912091c616156d5c34dbf3baa15f794c54
Fablicator <accts.fablicator@gmail.com>
2020-01-20 20:55:59 -0500

    Go back to classic jerk limiting and lower step pulse width

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1545,7 +1545,7 @@
  *
  * Override the default value based on the driver type set in Configuration.h.
  */
-#define MINIMUM_STEPPER_PULSE 6
+#define MINIMUM_STEPPER_PULSE 2
 
 /**
  * Maximum stepping rate (in Hz) the stepper driver allows
