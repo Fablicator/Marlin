8f87493b65cad7e57baf89bfa0c6f69d9be3b7ca
Karl Calden <kcalden323@gmail.com>
2020-06-09 14:38:57 -0500

    Revert change to Steps per unit in configuration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -753,7 +753,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 78.74, 78.74*2.0, 806, 117 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 78.74, 78.74, 806, 117 }
 
 /**
  * Default Max Feed Rate (mm/s)
