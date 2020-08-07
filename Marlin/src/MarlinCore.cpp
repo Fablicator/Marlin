c9ae3f6b568296cdd3feea11e9532500c319aae9
Karl Calden <kcalden323@gmail.com>
2020-08-07 16:00:08 -0400

    FIx "redeclaration of 'uint32_t serial_connect_timeout'"

--- Marlin/src/MarlinCore.cpp
+++ Marlin/src/MarlinCore.cpp
@@ -899,18 +899,6 @@ void setup() {
     #endif
   #endif
 
-  #if NUM_SERIAL > 0
-    MYSERIAL0.begin(BAUDRATE);
-    uint32_t serial_connect_timeout = millis() + 1000UL;
-    while (!MYSERIAL0 && PENDING(millis(), serial_connect_timeout)) { /*nada*/ }
-    #if HAS_MULTI_SERIAL
-      MYSERIAL1.begin(BAUDRATE);
-      serial_connect_timeout = millis() + 1000UL;
-      while (!MYSERIAL1 && PENDING(millis(), serial_connect_timeout)) { /*nada*/ }
-    #endif
-    SERIAL_ECHO_MSG("start");
-  #endif
-
   SETUP_RUN(HAL_init());
 
   #if HAS_L64XX
