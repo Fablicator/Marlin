2c1b992310878ae8eaa1b9b9a090a0177418b594
Karl Calden <33008330+kcalden@users.noreply.github.com>
2020-08-09 23:13:20 -0400

    Add ControlF4 pins to pass sanity check

--- Marlin/src/pins/stm32f4/pins_KL_CONTROLF4.h
+++ Marlin/src/pins/stm32f4/pins_KL_CONTROLF4.h
@@ -89,3 +89,12 @@
 #define SDIO_CMD_PIN    PD2
 
 #define SDCARD_CONNECTION   ONBOARD
+
+//
+// Required for sanity check
+#define X_ENABLE_PIN 0
+#define X2_ENABLE_PIN 0
+#define Y_ENABLE_PIN 0
+#define Z_ENABLE_PIN 0
+#define E0_ENABLE_PIN 0
+#define E1_ENABLE_PIN 0
