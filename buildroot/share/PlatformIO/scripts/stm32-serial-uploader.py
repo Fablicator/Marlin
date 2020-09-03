8866586c67dc3320ed5e968285f9cf3a02f0d472
Karl Calden <kcalden323@gmail.com>
2020-09-03 11:19:38 -0400

    Add stm32 serial uploaders

--- buildroot/share/PlatformIO/scripts/stm32-serial-uploader.py
+++ buildroot/share/PlatformIO/scripts/stm32-serial-uploader.py
@@ -1,6 +1,9 @@
+import sys
 Import("env")
 
-env.Replace(
-    UPLOADER="$PROJECT_DIR\\buildroot\\share\\PlatformIO\\scripts\\stm32-serial-uploader.exe",
-    UPLOADCMD="$UPLOADER --mcu-type $UPLOAD_FLAGS --port $UPLOAD_PORT --binary $BUILD_DIR/${PROGNAME}.bin"
-)
+if sys.platform.startswith("win32"): 
+    env.Replace(UPLOADER = "$PROJECT_DIR\\buildroot\\share\\PlatformIO\\scripts\\stm32-serial-uploader\\win32\\stm32-serial-uploader.exe")
+else if sys.platform.startswith("linux")
+    env.Replace(UPLOADER = "$PROJECT_DIR/buildroot/share/PlatformIO/scripts/stm32-serial-uploader/linux/stm32-serial-uploader")
+
+env.Replace(UPLOADCMD="$UPLOADER --mcu-type $UPLOAD_FLAGS --port $UPLOAD_PORT --binary $BUILD_DIR/${PROGNAME}.bin")
